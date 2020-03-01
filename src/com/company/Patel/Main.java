5b004a02946e6a5f881e75914e93a28c35ff8c3c
Sagar <sagarpatel1551@icloud.com>
2020-03-01 15:50:02 -0500

    Update 2

--- src/com/company/Patel/Main.java
+++ src/com/company/Patel/Main.java
@@ -49,16 +49,16 @@ public class Main {
                 }
             } else if (response == 4) {
                 int x = tasks.size();
-                for (int i = 0; i < x; i++) {
-                    System.out.println(tasks.get(i));
+                for (Task task : tasks) {
+                    System.out.println(task);
                 }
             } else if (response == 5) {
                 System.out.println("What is the priority of the task?");
                 int priority = input.nextInt();
                 int x = tasks.size();
-                for (int i = 0; i < x; i++) {
-                    if (priority == tasks.get(i).getPriority()) {
-                        System.out.println(tasks.get(i));
+                for (Task task : tasks) {
+                    if (priority == task.getPriority()) {
+                        System.out.println(task);
                     }
                 }
             } else {
